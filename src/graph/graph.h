9646630bb86673899386cf5713b0276a3fcfec68
rodolfo <rodolforeetz@gmail.com>
2025-05-18 07:56:32 -0300

    Adicionando os headers que eu tinha esquecido

--- src/graph/graph.h
+++ src/graph/graph.h
@@ -3,6 +3,7 @@
 
 #include "../point/point.h"
 
+typedef struct edge Edge;
 typedef struct graph Graph;
 
 /**
@@ -13,5 +14,76 @@ typedef struct graph Graph;
  * @return Grafo inicializado com os vértices
  */
 Graph *graph_construct(Point **vertices, int size);
+
+/**
+ * @brief Preenche a matriz com as distâncias entre os pontos
+ *
+ * @param g Estrutura que contém o conjunto de pontos para calcular a matriz
+ */
+void set_edges_matrix(Graph *g);
+
+/**
+ * @brief Retorna uma aresta entre os vértices i e j
+ *
+ * @param e Vetor contendo as arestas
+ * @param i Vértice i
+ * @param j Vértice j
+ * @return Distância entre os pontos i e j
+ */
+double get_edge_weight(Edge *e, int i, int j, int size);
+
+/**
+ * @brief Retorna o vetor com as arestas ordenadas.
+ *
+ * @param g Estrutura que contém o vetor de arestas ordenadas
+ * @return Vetor de arestas ordenadas
+ */
+Edge *get_sorted_edges(Graph *g);
+
+/**
+ * @brief Retorna o peso de uma aresta ordenada
+ *
+ * @param g Estrutra que contém o vetor de arestas ordenadas
+ * @param i Índice da aresta buscada
+ * @return double
+ */
+double get_sorted_edge_weight(Graph *g, int i);
+
+/**
+ * @brief Mapeia uma matriz de arestas em um índice de um vetor unidimensional
+ *
+ * @param i Índice da linha da matriz
+ * @param j Índice da coluna da matriz
+ * @param size Quantidade de linhas/colunas da matriz quadrada
+ * @return Retorna o valor do elemento (i,j) chaveado em um vetor
+ *
+ * A ideia dessa função é possibilitar o armazenamento das arestas em um vetor
+ * unidimensional, visto que em uma matriz size * size, as arestas só ocupariam
+ * o equivalente a uma matriz triangular sem a diagonal principal. Então,
+ * mapeando esses valores em um vetor menor, economizamos em número de alocações
+ * e possível custo de acesso.
+ */
+int map_edge_matrix(int i, int j, int size);
+
+/**
+ * @brief Função para ordenação das arestas
+ *
+ * @param g Estrutura que contém o vetor de arestas
+ */
+void sort_edges(Graph *g);
+
+/**
+ * @brief Inicializa o vetor de arestas ordenado
+ *
+ * @param g Estrutura com o vetor de arestas ordenado
+ */
+void set_sorted_edges(Graph *g);
+
+/**
+ * @brief Função que libera o espaço da estrutura
+ *
+ * @param g Estrura contendo os vértices e arestas para ser desalocada
+ */
+void graph_destroy(Graph *g);
 
 #endif
--- src/point/point.h
+++ src/point/point.h
@@ -12,48 +12,48 @@ typedef struct point Point;
  * @param id_len Tamanho do identificador do ponto.
  * @returns Ponteiro para o ponto com coordenada e identificador atribuídos.
  */
-Point *point_construct(int dimension, float *coordinate, char *id, int id_len);
+Point *point_construct(int dimension, double *coordinate, char *id, int id_len);
 
 /**
  * @brief Retorna a dimensão do ponto.
- * 
+ *
  * @param p Ponto que vai ter a dimensão retornada.
  * @returns Dimensão do ponto.
  */
- int get_dimension_point(Point *p);
+int get_dimension_point(Point *p);
 
- /**
-  * @brief Retorna as coordenadas do ponto.
-  * 
-  * @param p Ponto que vai ter as coordenadas retornadas.
-  * @return Coordenadas do ponto.
-  */
- float *get_coordinate_point(Point *p);
+/**
+ * @brief Retorna as coordenadas do ponto.
+ *
+ * @param p Ponto que vai ter as coordenadas retornadas.
+ * @return Coordenadas do ponto.
+ */
+double *get_coordinate_point(Point *p);
 
- /**
-  * @brief Retorna o ID único do ponto
-  * 
-  * @param p Ponto que vai ter o ID retornado.
-  * @return ID único do ponto.
-  */
- char *get_id_point(Point *p);
+/**
+ * @brief Retorna o ID único do ponto
+ *
+ * @param p Ponto que vai ter o ID retornado.
+ * @return ID único do ponto.
+ */
+char *get_id_point(Point *p);
 
 /**
  * @brief Retorna o tamanho do ID único do ponto.
- * 
+ *
  * @param p Ponto que vai ter o tamanho do ID retornado.
  * @return Tamanho do ID do ponto.
  */
- int get_id_len_point(Point *p);
+int get_id_len_point(Point *p);
 
- /**
-  * @brief Retorna a distância euclidiana entre dois pontos
-  * 
-  * @param p1 Ponto 1.
-  * @param p2 Ponto 2.
-  * @return Distância euclidiana entre os dois pontos passados como parâmetro
-  */
- double two_points_distance(Point *p1, Point *p2);
+/**
+ * @brief Retorna a distância euclidiana entre dois pontos
+ *
+ * @param p1 Ponto 1.
+ * @param p2 Ponto 2.
+ * @return Distância euclidiana entre os dois pontos passados como parâmetro
+ */
+double two_points_distance(Point *p1, Point *p2);
 
 /**
  * @brief Função que libera o espaço alocado para o ponto.
